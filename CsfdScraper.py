465ea243cf750db511d8ab939c5a92ae92d72142
Marek Štec <mar.stec@seznam.cz>
2023-06-12 12:39:22 +0200

    CsfdScraper fixed

--- CsfdScraper.py
+++ CsfdScraper.py
@@ -1,40 +1,77 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 
-def get_available_streaming(list_name):
-    available = []
-    with open(list_name, "r", encoding="utf-8") as f:
-        available = [item.lower().strip() for item in f.readlines()]
-    return available
+class CsfdScraper:
+    def get_available_streaming(self, list_name):
+        available = []
+        with open(list_name, "r", encoding="utf-8") as f:
+            available = [item.lower().strip() for item in f.readlines()]
+        return available
 
-
-def get_film_data(url):
-    s = requests.Session()
-    headers = {
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-    }
-    s.headers = headers
-    html = s.get(url)
-    available_straming = get_available_streaming("available_streaming.txt")
-    if html.status_code == 200:
-        soup = BeautifulSoup(html.content, "html.parser")
-        # get rating
-        rating = soup.find("div", class_="film-rating-average").get_text().strip()
-        # get streaming services
-        streaming_div = soup.find("div", class_="box-film-vod-services")
-        streaming_services = streaming_div.find_all("div", class_="vod-item")
-        streaming_services_list = []
-        for item in streaming_services:
-            item = item.get_text().strip().lower()
-            if item in available_straming:
-                streaming_services_list.append(item)
-
-        return (rating, streaming_services_list)
-    else:
+    def get_film_data(self, url):
+        s = requests.Session()
+        headers = {
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        }
+        s.headers = headers
+        html = s.get(url)
+        available_straming = self.get_available_streaming("available_streaming.txt")
+        if html.status_code == 200:
+            soup = BeautifulSoup(html.content, "html.parser")
+            # get rating
+            try:
+                rating = (
+                    soup.find("div", class_="film-rating-average").get_text().strip()
+                )
+                rating = rating.replace("%", "")
+            except:
+                raiting = None
+            # get streaming services
+            streaming_services_list = []
+            try:
+                streaming_div = soup.find("div", class_="box-film-vod-services")
+                streaming_services = streaming_div.find_all("div", class_="vod-item")
+                for item in streaming_services:
+                    item = item.get_text().strip().lower()
+                    if item in available_straming:
+                        streaming_services_list.append(item)
+            except:
+                streaming_services_list = []
+            return (rating, streaming_services_list)
         return None
 
+    def get_all_film_data(self, films_csv):
+        scraped_data = dict()
+        failed = dict()
+        with open(films_csv, "r", encoding="utf-8") as f:
+            file = f.readlines()
+            for i in range(1, len(file)):
+                film, link = list(map(str.strip, file[i].split(",")))
+                curr_data = self.get_film_data(link)
+                if curr_data:
+                    rating, streaming = curr_data
+                    data = (rating, streaming, link)
+                    scraped_data.update({film: data})
+                else:
+                    failed.update({film: link})
 
-url = "https://www.csfd.cz/film/1626-harry-potter-a-kamen-mudrcu/prehled/"
-data = get_film_data(url)
-print(data)
+        output_csv = "scraped_data.csv"
+
+        if os.path.exists(output_csv):
+            with open(output_csv, "a", encoding="utf-8") as f:
+                for key, value in scraped_data.items():
+                    f.write(f"{key},{value[0]},{value[1]},{value[2]}\n")
+        else:
+            with open(output_csv, "w", encoding="utf-8") as f:
+                f.write("Název,Rating,Streaming,Odkaz\n")
+                for key, value in scraped_data.items():
+                    f.write(
+                        f"{key},{value[0]},{value[1] if value[1] else None},{value[2]}\n"
+                    )
+
+        with open("failed.csv", "w", encoding="utf-8") as f:
+            f.write("Název,Odkaz\n")
+            for key, value in failed.items():
+                f.write(f"{key},{value}\n")
--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 from UrlGetter import UrlGetter
+from CsfdScraper import CsfdScraper
 import os
 import concurrent.futures
 
 
 if __name__ == "__main__":
-    url_getter = UrlGetter()
-    films = url_getter.get_films("new.txt")
-    number_of_threads = min(900, len(films))
-    results = []
+    # url_getter = UrlGetter()
+    # films = url_getter.get_films("new.txt")
+    # number_of_threads = min(900, len(films))
+    # results = []
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        divided_list = url_getter.divide_into_threads(number_of_threads, films)
-        results = [
-            executor.submit(url_getter.get_all_urls, divided_list[x])
-            for x in range(len(divided_list))
-        ]
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     divided_list = url_getter.divide_into_threads(number_of_threads, films)
+    #     results = [
+    #         executor.submit(url_getter.get_all_urls, divided_list[x])
+    #         for x in range(len(divided_list))
+    #     ]
 
-    url_file = "urls_new.csv"
-    if os.path.exists(url_file):
-        with open(url_file, "a", encoding="utf-8") as f:
-            for key, value in url_getter.urls.items():
-                f.write(f"{key}, {value}\n")
-    else:
-        with open(url_file, "w", encoding="utf-8") as f:
-            f.write("Název,Odkaz\n")
-            for key, value in url_getter.urls.items():
-                f.write(f"{key}, {value}\n")
+    # url_file = "urls_new.csv"
+    # if os.path.exists(url_file):
+    #     with open(url_file, "a", encoding="utf-8") as f:
+    #         for key, value in url_getter.urls.items():
+    #             f.write(f"{key}, {value}\n")
+    # else:
+    #     with open(url_file, "w", encoding="utf-8") as f:
+    #         f.write("Název,Odkaz\n")
+    #         for key, value in url_getter.urls.items():
+    #             f.write(f"{key}, {value}\n")
 
-    with open("failed.txt", "w", encoding="utf-8") as f:
-        for item in url_getter.failed:
-            f.write(item)
-            f.write("\n")
+    # with open("failed.txt", "w", encoding="utf-8") as f:
+    #     for item in url_getter.failed:
+    #         f.write(item)
+    #         f.write("\n")
 
-    print("finished")
+    # print("finished")
+
+    csfd_scraper = CsfdScraper()
+    csfd_scraper.get_all_film_data("test.csv")
